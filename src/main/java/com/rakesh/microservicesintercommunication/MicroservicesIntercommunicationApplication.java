d0a47841f0cc61629f3d964f2a152ee61f611007
Rakesh Gowda <rakeshgowda3101@gmail.com>
2023-05-30 14:36:50 +0530

    initial commit

---
+++ src/main/java/com/rakesh/microservicesintercommunication/MicroservicesIntercommunicationApplication.java
@@ -0,0 +1,13 @@
+package com.rakesh.microservicesintercommunication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesIntercommunicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesIntercommunicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rakesh/microservicesintercommunication/MicroservicesIntercommunicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rakesh.microservicesintercommunication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesIntercommunicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
